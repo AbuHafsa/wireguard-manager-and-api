29a5f5c3b61b673ec451c113588d38e7f98a44f1
Mohamed Houssein Douici <douici1@gmail.com>
2022-06-05 01:52:32 +0200

    test auth middleware verification header

--- src/api/api_test.go
+++ src/api/api_test.go
@@ -6,25 +6,40 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/spf13/viper"
 	"gitlab.com/raspberry.tech/wireguard-manager-and-api/src/api/middleware"
 )
 
-func TestAuthMiddleware(t *testing.T) {
-	req, err := http.NewRequest(http.MethodGet, "/manager/key", nil)
+func setUpConfigurationFile() {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("json")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/opt/wgManagerAPI/")
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic("Unable to read configuration file")
+	}
+}
+
+func Test_Returns_BadRequest_When_Invalid_Authentication_Key(t *testing.T) {
+	setUpConfigurationFile()
+	viper.Set("SERVER.AUTH", "eyJhbGciOiJIUzI1NiJ9")
+
+	req, err := http.NewRequest(http.MethodGet, "", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		middleware.Respond(w)
+		middleware.VerifyAuthorizationHeader(w, r)
 	})
 
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusBadRequest {
 		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+			status, http.StatusBadRequest)
 	}
 
 	expected := middleware.FailedAuthentication{
@@ -35,9 +50,32 @@ func TestAuthMiddleware(t *testing.T) {
 	var got middleware.FailedAuthentication
 	err = json.Unmarshal(rr.Body.Bytes(), &got)
 	if err != nil {
-		t.Errorf("unable to parse body")
+		t.Errorf("unable to parse body %v", err)
 	}
 	if got.StatusCode != expected.StatusCode {
 		t.Errorf("handler returned wrong status code: got %v want %v", got.StatusCode, expected.StatusCode)
+	}
+}
+
+func Test_Returns_StatusOK_When_Valid_Authentication_Key(t *testing.T) {
+	setUpConfigurationFile()
+	viper.Set("SERVER.AUTH", "eyJhbGciOiJIUzI1NiJ9")
+
+	req, err := http.NewRequest(http.MethodGet, "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		middleware.VerifyAuthorizationHeader(w, r)
+	})
+
+	req.Header.Set("Authorization", "eyJhbGciOiJIUzI1NiJ9")
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
 	}
 }
--- src/api/middleware/auth.go
+++ src/api/middleware/auth.go
@@ -12,27 +12,27 @@ type FailedAuthentication struct {
 	Message    string `json:"message"`
 }
 
-func AuthMiddleware(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		auth := viper.GetString("SERVER.AUTH")
-		if auth != "-" { //check AUTH
-			authHeader := req.Header.Get("Authorization")
-			if auth != authHeader {
-				Respond(res)
-				return
-			}
-		}
+func VerifyAuthorizationHeader(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
 
-		handler.ServeHTTP(res, req)
-	})
+	auth := viper.GetString("SERVER.AUTH")
+	if auth != "" { //check AUTH
+		authHeader := req.Header.Get("Authorization")
+		if auth != authHeader {
+			res.WriteHeader(http.StatusBadRequest)
+			payload, _ := json.Marshal(FailedAuthentication{
+				StatusCode: 400,
+				Message:    "Invalid authentication key",
+			})
+			res.Write(payload)
+			return
+		}
+	}
 }
 
-func Respond(res http.ResponseWriter) {
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusBadRequest)
-	payload, _ := json.Marshal(FailedAuthentication{
-		StatusCode: 400,
-		Message:    "Invalid authentication key",
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		VerifyAuthorizationHeader(res, req)
+		next.ServeHTTP(res, req)
 	})
-	res.Write(payload)
 }
