bf2f9522f5b6833259e7d010fcec4faeed57e2d1
Mohamed Houssein Douici <douici1@gmail.com>
2022-07-26 02:33:21 +0200

    introduce hyperlite and refactor some handlers using the framework

--- src/api/server.go
+++ src/api/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	. "hyperlite"
 	"net"
 	"net/http"
 
@@ -13,6 +14,8 @@ type authStruct struct {
 	Active bool `json:"active"`
 }
 
+var app *App
+
 func RunServer() {
 	combinedLogger := logger.GetCombinedLogger()
 	combinedLogger.Info("Starting web server")
@@ -20,20 +23,37 @@ func RunServer() {
 	routes := router.InitializeRoutes()
 
 	serverDev := viper.GetBool("SERVER.SECURITY")
-	port := viper.GetString("SERVER.PORT")
+	port := viper.GetInt("SERVER.PORT")
 	fullchainCert := viper.GetString("SERVER.CERT.FULLCHAIN")
 	privKeyCert := viper.GetString("SERVER.CERT.PK")
-	resolve, _ := net.ResolveTCPAddr("tcp4", "0.0.0.0:"+port)
+	resolve, _ := net.ResolveTCPAddr("tcp4", "0.0.0.0:"+string(port))
 	resolveTCP, _ := net.ListenTCP("tcp4", resolve)
 
-	combinedLogger.Info("HTTP about to listen on " + port)
+	combinedLogger.Info("HTTP about to listen on " + string(port))
 
-	var err error
+	var _ error
 	if !serverDev {
-		err = http.Serve(resolveTCP, routes)
+		app = NewApp(AppConfig{
+			Server: &ServerConfig{
+				Host: "0.0.0.0",
+				Port: port,
+			},
+		})
+		app.NotFoundContext = func(c *Context) {
+			c.JSONError(404, "Endpoint not defined")
+		}
+		DefineRoutes()
+		app.Start()
+		//http.Serve(resolveTCP, routes)
 	} else {
-		err = http.ServeTLS(resolveTCP, routes, fullchainCert, privKeyCert)
+		http.ServeTLS(resolveTCP, routes, fullchainCert, privKeyCert)
 	}
 
-	combinedLogger.Error("Failed to startup of API server " + err.Error())
+	//combinedLogger.Error("Failed to startup of API server " + err.Error())
+}
+
+func DefineRoutes() {
+	app.Get("/manager/key", router.GetKeys)
+	app.Post("/manager/key", router.CreateKey)
+	app.Delete("/manager/key", router.DeleteKey)
 }
